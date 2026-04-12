1b8eddcbcd10d35d92bda66d79201393657584f5
unknown <farhananwari1999@gmail.com>
2026-04-12 22:07:55 +0700

    update master paket

---
+++ models/master_paket_barang_model.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"time"
+)
+
+type MasterPaketBarang struct {
+	ID         string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	KodeBarang string    `gorm:"type:varchar(100);unique;not null;" json:"kode_barang"`
+	Nama       string    `gorm:"type:varchar(100);not null;" json:"nama"`
+	BarangID   string    `gorm:"type:varchar(100);not null;" json:"barang_id"`
+	IsActive   bool      `gorm:"type:boolean;default:true;" json:"is_active"`
+	CreatedBy  string    `gorm:"type:varchar(100);not null;" json:"created_by"`
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
+
+type MasterPaketBarangDetail struct {
+	ID                  string    `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	MasterPaketBarangID string    `gorm:"type:varchar(100);not null;" json:"master_paket_barang_id"`
+	BarangID            string    `gorm:"type:varchar(100);not null;" json:"barang_id"`
+	Jumlah              int       `gorm:"type:int;not null;" json:"jumlah"`
+	CreatedBy           string    `gorm:"type:varchar(100);not null;" json:"created_by"`
+	CreatedAt           time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt           time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
